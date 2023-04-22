3fe969011ebc0c332d954fd73e355d4753041602
Jesus <jesus.fdz93@gmail.com>
2023-04-22 12:38:54 -0500

    actualizacion

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 import { llamarUsuarios, saludar } from "./funciones.js";
 import frutas from "./data.js";
+console.log(frutas);
 
 const btnSaludar = document.getElementById("btnSaludar");
 
 btnSaludar.addEventListener("click", saludar);
-console.log(frutas);
-
 window.addEventListener("load", llamarUsuarios);
